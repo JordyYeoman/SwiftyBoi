0254a494b7e5f5beee8619d63095b32e63bc61d9
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2023-05-26 16:45:38 +0800

    chore: lesson 1 continued

--- Lesson1/Lesson1.playground/Contents.swift
+++ Lesson1/Lesson1.playground/Contents.swift
@@ -9,3 +9,32 @@ var greeting2 = "Hello lil homie"
 print(greeting2)
 
 // Full send
+
+// Constants in swift are defined with the let keyword
+let name = "Jordy Yeoman"
+var street = "Hangar 7, Veldin"
+print(street)
+street = "Kyzil Plateau"
+print(street)
+
+struct Life {
+    var initialLife = 0
+    var currentLife = 0
+    let name: String
+    let street: String
+    
+    func sayHello() {
+        print("Hello there! My name is \(name) and I'm from: \(street).")
+    }
+    
+    func fookinSendIt() {
+        print("Yeh nah yeah nah... yeh nahhhh")
+    }
+}
+
+let j = Life(initialLife: 0, currentLife: 0, name: name, street: street)
+print(j.street)
+print(j.initialLife)
+j.sayHello()
+j.fookinSendIt()
+
