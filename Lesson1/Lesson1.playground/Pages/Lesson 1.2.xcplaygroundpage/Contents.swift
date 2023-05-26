307d3cca3ebc372fe73d2b00e5c146aacfdb46e9
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2023-05-26 21:11:26 +0800

    chore: further learnigns

---
+++ Lesson1/Lesson1.playground/Pages/Lesson 1.2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let favouritePerson = "JSwizzle"
+
+// Switch example
+var x = 1
+
+switch x {
+case 0:
+    print("Nahhhh")
+case 1:
+    print("Yeahhhh")
+default:
+    print("tis broken")
+}
