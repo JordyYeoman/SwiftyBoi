010704d7b05aad4daa78c3bcae00d2b96385a007
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2023-06-06 19:13:08 +0800

    chore: initial swift project setup

---
+++ Lesson1/GettingStartOld/GettingStartOld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GettingStartOld
+//
+//  Created by Jordy Yeoman on 6/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lesson1/GettingStarted/GettingStarted/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GettingStarted
+//
+//  Created by Jordy Yeoman on 6/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
