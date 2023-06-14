411ed389e0eabb212c83c880bb02716bec5424aa
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-14 17:31:13 +0800

    chore: day4 function params

--- HackingWithSwift100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
+++ HackingWithSwift100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
@@ -8,3 +8,12 @@ func testMethod(input: String, num: Int) -> Int {
 
 let res = testMethod(input: "Send it", num: 6)
 print(res)
+
+// Swift also gives us the option to use labels
+// Think of labels as the param name that is externally facing
+// eg
+func sendIt(to whereToSendIt: String) -> Void {
+    print("Sending it to \(whereToSendIt)")
+}
+
+sendIt(to: "The Moon!!")
