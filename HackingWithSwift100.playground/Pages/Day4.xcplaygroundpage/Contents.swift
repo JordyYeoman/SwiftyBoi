12870cb5d5c569f5b91acb3f07d15d3c58326b11
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-15 22:42:49 +0800

    chore: day6 closures

--- HackingWithSwift100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
+++ HackingWithSwift100.playground/Pages/Day4.xcplaygroundpage/Contents.swift
@@ -17,3 +17,45 @@ func sendIt(to whereToSendIt: String) -> Void {
 }
 
 sendIt(to: "The Moon!!")
+
+
+// Basic closure
+let jordySendIt = {
+    print("Alright, lets go!!")
+}
+
+jordySendIt()
+
+// Closures taking arguments
+
+let runCode = { (language: String) in
+    print("Executing code in \(language) & appropriate runtime.")
+}
+
+// Difference between closures and normal functions, is closures remove the need for an argument label.
+runCode("Swift")
+
+// Closures that return a value
+let driving = { (place: String) -> String in
+    return "I'm driving to \(place)."
+}
+
+let newHome = driving("Hello")
+print(newHome)
+
+// Passing a closure as a function param
+// function
+func doSomethingMagical(action: () -> Void) {
+    print("Doing some magic trick...")
+    action()
+}
+
+// Closure
+let sleightOfHand = {
+    print("Bra we just pulled a fast one on yah")
+}
+
+// pre cool!!
+// Calling the function by passing in the closure
+doSomethingMagical(action: sleightOfHand)
+
