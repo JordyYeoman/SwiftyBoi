386818a234e6d22521e3a38b1187b31b9b850c7d
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-18 08:42:27 +0800

    chore: basic weather app

--- StarterPack/StarterPack/ContentView.swift
+++ StarterPack/StarterPack/ContentView.swift
@@ -2,8 +2,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("No sir, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            // Vstack will only take up as much space as it needs
+            VStack(spacing: 10) {
+                Text("Perth, WA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180, alignment: .center)
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
