46ace5680663777f438feb427d4f749f6d063c56
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-25 16:08:44 +0800

    chore: send it

--- JSwizzle-Pokedex/JSwizzle-Pokedex/Api/Pokemon.swift
+++ JSwizzle-Pokedex/JSwizzle-Pokedex/Api/Pokemon.swift
@@ -1,7 +1,5 @@
 import Foundation
 
-// https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151
-
 struct Pokemon: Codable {
     var results: [PokemonEntry]
 }
@@ -9,5 +7,16 @@ struct Pokemon: Codable {
 // Codable - From the AppleDocs - A type that can convert itself into and out of an external representation.
 // Identifiable - "" ^ - hold the value of an entity with stable identity.
 struct PokemonEntry: Codable, Identifiable {
-    
+    let id = UUID()
+    let name: String
+    let url: String
+}
+
+class PokeApi {
+    func getData(completion: @escaping ([PokemonEntry]) -> ()) {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
+        else {
+            return
+        }
+    }
 }
