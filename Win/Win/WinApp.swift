4c78dcbcc0b51ec78e3ae7b09e30ecc9efc98fce
Jordy Yeoman <jordyyeoman@gmail.com>
2023-08-26 15:41:54 +0800

    feat: adding new project files

---
+++ Win/Win/WinApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinApp.swift
+//  Win
+//
+//  Created by Jordy Yeoman on 26/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct WinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
