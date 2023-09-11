fbbe57ca5e5030584883ee430b9ba5ef873e077f
Jordy Yeoman <jordyyeoman@gmail.com>
2023-09-11 17:25:03 +0800

    fix: sending it

--- HackingWithSwift100.playground/Pages/ReadingTextFile.xcplaygroundpage/Contents.swift
+++ HackingWithSwift100.playground/Pages/ReadingTextFile.xcplaygroundpage/Contents.swift
@@ -30,3 +30,89 @@ func readFile(inputFileName: String, fileExtension: String) -> [String] {
 }
 
 var myData = readFile(inputFileName: "testing", fileExtension: "txt")
+
+struct deque<T> {
+    var array = [T]()
+    var count: Int {
+        return array.count
+    }
+    var arraySummed: Int {
+        if let intArray = array as? [Int] {
+            return intArray.reduce(0, { x, y in x + y })
+        } else {
+            return 0
+        }
+    }
+    
+    mutating func pushFront(_ obj: T) {
+        array.insert(obj, at: 0)
+    }
+    
+    mutating func pushBack(_ obj: T) {
+        array.append(obj)
+    }
+    
+    mutating func popBack() -> T? {
+        return array.popLast()
+    }
+    
+    mutating func popFront() -> T? {
+        if array.first == nil {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+    
+    mutating func solveSubArr(numToAchieve: Int) -> Int {
+        while count > 0 {
+            if(arraySummed == numToAchieve) {
+                return arraySummed
+            } else {
+                self.popBack()
+            }
+        }
+        
+        return -1
+    }
+}
+
+var testNumbers = deque<Int>()
+testNumbers.pushFront(3)
+testNumbers.pushBack(2)
+testNumbers.pushFront(1)
+print(testNumbers)
+testNumbers.popFront()
+print(testNumbers)
+
+// Given an integer array nums and an integer k,
+// return the length of the shortest non-empty subarray of nums with a sum of at least k.
+// If there is no such subarray, return -1.
+
+// A subarray is a contiguous part of an array.
+
+// Example 1:
+//
+// Input: nums = [1], k = 1
+// Output: 1
+// Example 2:
+//
+// Input: nums = [1,2], k = 4
+// Output: -1
+// Example 3:
+//
+// Input: nums = [2,-1,2], k = 3
+// Output: 3
+
+class Solution {
+    func shortestSubarray(_ nums: [Int], _ k: Int) -> Int {
+        // Implement a moving window until we find the smallest length of array that can satisfy our number
+        var dq = deque(array: nums)
+        return dq.solveSubArr(numToAchieve: k)
+    }
+}
+
+let sol = Solution()
+print(sol.shortestSubarray([1], 1))
+print(sol.shortestSubarray([1, 2], 4))
+print(sol.shortestSubarray([2,-1,2], 3))
