4b159976d89f95e27c1c4e3a5edd344c443dc063
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-14 09:06:53 +0800

    feat: testing swift concurrency

--- SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/DoCatchTryThrowBootcCamp.swift
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/DoCatchTryThrowBootcCamp.swift
@@ -15,11 +15,29 @@ class DoCatchTryThrowsBootcampDataManager {
     
     let isActive: Bool = false
     
-    func getTitle() -> String? {
+    func getTitle() -> (title: String?, error: Error?) {
         if isActive {
-            return "NEW TITLE!!!"
+            return ("NEW TITLE!!!", nil)
         } else {
-            return nil
+            return (nil, URLError(.badURL))
+        }
+    }
+    
+    // Improved version
+    func getTitle2() -> Result<String, Error> {
+        if isActive {
+            return .success("NEW TITLE!!!")
+        } else {
+            return .failure(URLError(.badURL))
+        }
+    }
+    
+    // Improved further with throws
+    func getTitle3() throws -> String {
+        if isActive {
+            return "NEW TEXT AGAIN!!"
+        } else {
+            throw URLError(.badServerResponse)
         }
     }
 }
@@ -29,10 +47,32 @@ class DoCatchTryThrowBootCampViewModel: ObservableObject {
     @Published var text: String = "Starting text..."
     let manager = DoCatchTryThrowsBootcampDataManager()
     
+    //    func fetchTitle() {
+    //        let returnedValue = manager.getTitle()
+    //        if let newTitle = returnedValue.title {
+    //            self.text = newTitle
+    //        } else if let error = returnedValue.error {
+    //            self.text = error.localizedDescription
+    //        }
+    //    }
+    
+    //    func fetchTitle() {
+    //        let result = manager.getTitle2()
+    //
+    //        switch(result) {
+    //        case .success(let newTitle):
+    //            self.text = newTitle
+    //        case .failure(let error):
+    //            self.text = error.localizedDescription
+    //        }
+    //    }
+    
     func fetchTitle() {
-        let newTitle = manager.getTitle()
-        if let newTitle = newTitle {
+        do {
+            let newTitle = try manager.getTitle3()
             self.text = newTitle
+        } catch {
+            self.text = error.localizedDescription
         }
     }
 }
