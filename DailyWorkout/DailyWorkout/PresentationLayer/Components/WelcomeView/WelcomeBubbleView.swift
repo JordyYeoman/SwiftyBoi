8bca8e18abe04e20cdc4d9b133f86aa876ec9ab9
Jordy Yeoman <jordyyeoman@gmail.com>
2025-03-10 15:03:45 +0800

    feat: add home view metrics cards

---
+++ DailyWorkout/DailyWorkout/PresentationLayer/Components/WelcomeView/WelcomeBubbleView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct WelcomeBubbleView: View {
+    @State var viewModel: HomeViewModel
+    
+    var body: some View {
+        if !viewModel.hasDismissedWelcomeText {
+            HStack {
+                Text(
+                    "Welcome \(viewModel.isFirstLaunch ? "" : "back,") \(viewModel.username)"
+                )
+                .onAppear {
+                    viewModel.isFirstLaunch = false
+                }
+                .font(.title2)
+                .fontWeight(.medium)
+                .padding()
+                .foregroundStyle(Color.retroDarkPurple)
+                Spacer()
+                Button {
+                    viewModel.hasDismissedWelcomeText.toggle()
+                } label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 12, height: 12)
+                }
+                .padding(.trailing, 24)
+                .foregroundStyle(Color.retroDarkPurple)
+            }
+            .padding(.horizontal, 12)
+            .background(Color(UIColor.secondarySystemBackground))
+        }
+    }
+}
